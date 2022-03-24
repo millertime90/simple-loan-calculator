5a39f243d7055b00ce64f22cbd44b3337e64ff05
millertime90 <scott.j.miller0727@gmail.com>
2022-03-23 21:39:51 -0500

    updated site.js and code.html
    Still need to break down and explain code in code.html

--- js/site.js
+++ js/site.js
@@ -1,80 +1,22 @@
-// CONTROLLER/VIEW FUNCTION(s) 
-
-function displaySchedule() {
+function displayAllFigures() {
 
     let monthlyPayments = document.querySelector(".monthlyPayments>h1"); 
-    let totalPrincipal = document.querySelector("#totalPrincipal"); 
+    let totalInterest = document.querySelector("#totalInterest"); 
 
-    // LOGIC/VIEW FUNCTION RETURNED 
+    monthlyPayments.innerHTML = `$${getMonthlyPayment().toFixed(2)}`; 
+    getTotalPrincipal(); 
+    totalInterest.innerHTML = `$${getTotalInterest().reduce((a, b) => a + b, 0).toFixed(2)}`; 
+    getTotalCost(); 
+    getSchedule(); 
 
-    return () => { 
+}
 
-        // let interest = getInterestPayment(monthlyInterestRate, currentWholePrincipal); 
-        // let principal = getPrincipalPayment(getInterestPayment); 
+function getMonthlyPayment() {
 
-        // RENDER HTML HERE WITH A LOOP 
-
-        let getMonthFigures = function(currentMonth, monthlyPayment, getPrincipalPayment, gip, totalPrincipalPaid, totalInterestPaid, balance) {
-
-            let tr = document.createElement("TR"); 
-            tr.setAttribute("class", "text-center"); 
-            tr.innerHTML = 
-            `<td>${currentMonth}</td>
-            <td>${monthlyPayment.toFixed(2)}</td>
-            <td>${getPrincipalPayment(getInterestPayment).toFixed(2)}</td>
-            <td>${gip.toFixed(2)}</td>
-            <td>${totalPrincipalPaid}</td>
-            <td>${totalInterestPaid}</td>
-            <td>${balance.toFixed(2)}</td>`; 
-
-            return tr; 
-
-        }; 
-
-        let getInterestPayment = function(monthlyInterestRate, currentWholePrincipal) { 
-            
-            return monthlyInterestRate * currentWholePrincipal; 
-        
-        }
-
-        let getPrincipalPayment = getInterestPayment => {
-
-            let currentMonthInterest = getInterestPayment(monthlyInterestRate, currentWholePrincipal); 
-            return MONTHLY_PAYMENT - currentMonthInterest; 
+    let loanAmt = parseInt(document.querySelector("#loanAmt").value); 
+    let monthlyInterestRate = (parseInt(document.querySelector("#intRate").value) / 100) / 12; 
+    let term = parseInt(document.querySelector("#term").value); 
     
-        }; 
-
-        let intRate = Number.parseInt(document.querySelector("#intRate").value); 
-        let currentWholePrincipal = Number.parseInt(document.querySelector("#loanAmt").value);              // decrements per monthly payment 
-        let term = Number.parseInt(document.querySelector("#term").value); 
-        let monthlyInterestRate = (intRate / 100) / 12; 
-
-        const MONTHLY_PAYMENT = getMonthlyPayment(currentWholePrincipal, monthlyInterestRate, term); 
-
-        let results = document.querySelector("#results"); 
-        let monthlyPayment = getMonthlyPayment(currentWholePrincipal, monthlyInterestRate, term); 
-        let currentMonth = 1; 
-
-        monthlyPayments.innerHTML = "$" + MONTHLY_PAYMENT.toFixed(2); 
-        totalPrincipal.innerHTML = "$" + currentWholePrincipal.toFixed(2); 
-
-        for(let i = 0; i < term; i++) { 
-
-            let currentMonthInterest = getInterestPayment(monthlyInterestRate, currentWholePrincipal); 
-            let interestArg = getInterestPayment(monthlyInterestRate, currentWholePrincipal); 
-            currentWholePrincipal -= MONTHLY_PAYMENT - currentMonthInterest; 
-            results.append(getMonthFigures(currentMonth++, MONTHLY_PAYMENT, getPrincipalPayment, interestArg, "This needs a function", "This needs a function", currentWholePrincipal)); 
-
-        }
-
-    }; 
-
-} 
-
-// LOGIC FUNCTION(s) 
-
-function getMonthlyPayment(loanAmt, monthlyInterestRate, term) {
-
     let nominator = loanAmt * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, term)); 
     let denominator = Math.pow(1 + monthlyInterestRate, term) - 1; 
 
@@ -82,4 +24,120 @@ function getMonthlyPayment(loanAmt, monthlyInterestRate, term) {
 
 } 
 
-const DISPLAY_SCHEDULE_EXEC = displaySchedule(); 
+function getTotalPrincipal() {
+
+    let loanAmt = document.querySelector("#loanAmt"); 
+    let totalPrincipal = document.querySelector("#totalPrincipal"); 
+    totalPrincipal.innerHTML = `$${parseInt(loanAmt.value).toFixed(2)}`; 
+
+} 
+
+function getTotalInterest() {
+
+    let loanAmt = parseInt(document.querySelector("#loanAmt").value); 
+    let term = parseInt(document.querySelector("#term").value); 
+    let monthly_intRate = (parseInt(document.querySelector("#intRate").value) / 100) / 12; 
+    let interestPayments = []; 
+
+    for(let i = 0; i < term; i++) {
+
+        interestPayments.push(monthly_intRate * loanAmt); 
+        loanAmt -= getMonthlyPayment() - (monthly_intRate * loanAmt); 
+
+    } 
+
+    return interestPayments; 
+
+} 
+
+function getTotalCost() {
+
+    let loanAmt = parseInt(document.querySelector("#loanAmt").value); 
+    let totalCost = document.querySelector("#totalCost"); 
+
+    totalCost.innerHTML = `$${loanAmt + Number(getTotalInterest().reduce((a, b) => a + b, 0).toFixed(2))}`; 
+
+} 
+
+function getSchedule() {
+
+    let getMonthFigures = (month, payment, principal, interest, totalPrincipalPaid, totalInterestPaid, balance) => {
+
+        let tr = document.createElement("TR"); 
+        tr.setAttribute("class", "text-center"); 
+        tr.innerHTML = 
+        `<td>${month}</td>
+        <td>${payment}</td>
+        <td>${principal}</td>
+        <td>${interest}</td>
+        <td>${totalPrincipalPaid}</td>
+        <td>${totalInterestPaid}</td>
+        <td>${balance}</td>`; 
+
+        return tr; 
+
+    }; 
+
+    let term = parseInt(document.querySelector("#term").value); 
+    let balance = parseInt(document.querySelector("#loanAmt").value); 
+    let month = 1; 
+    let payment = getMonthlyPayment(); 
+    let intArray = getTotalInterest(); 
+
+    let principalArray = (() => {
+
+        let arr = []; 
+        for(let i = 0; i < intArray.length; i++) {
+
+            arr.push(payment - intArray[i]); 
+
+        } 
+
+        return arr; 
+
+    })(); 
+
+    let totalPrincipalPaid = (() => {
+
+        let arr = new Array(...principalArray); 
+        let arr1 = []; 
+        let arr2 = []; 
+
+        for(let i = 0; i < arr.length; i++) {
+
+            arr1.push(arr[i]); 
+            arr2.push([...arr1].reduce((a, b) => a + b, 0)); 
+
+        } 
+
+        return arr2; 
+
+    })(); 
+
+    let totalInterestPaid = (() => {
+
+        let arr = new Array(...intArray); 
+        let arr1 = []; 
+        let arr2 = []; 
+
+        for(let i = 0; i < arr.length; i++) {
+
+            arr1.push(arr[i]); 
+            arr2.push([...arr1].reduce((a, b) => a + b, 0)); 
+
+        } 
+
+        return arr2; 
+
+    })(); 
+
+    let results = document.querySelector("#results"); 
+
+    for(let i = 0; i < term; i++) { 
+
+        balance -= getMonthlyPayment() - intArray[i]; 
+        results.append(getMonthFigures(month++, getMonthlyPayment().toFixed(2), principalArray[i].toFixed(2), intArray[i].toFixed(2), totalPrincipalPaid[i].toFixed(2), totalInterestPaid[i].toFixed(2), balance.toFixed(2))); 
+
+    } 
+
+} 
